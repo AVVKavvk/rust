484a2b7565b66a80a810ba0f69bb05b858b02b64
AVVKavvk <kumawatvipin066@gmail.com>
2025-07-26 10:37:09 +0530

    REvise

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    let _a = 32;
+    // _a = 32
+    let mut guess = String::new();
+
+    guess.push_str("Vipin");
 }
